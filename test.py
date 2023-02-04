c16ae23645668733b4336f0be28ffa2704e1ba1c
Sabnam Pandit <sabnampandit@Sabnams-Air.lan>
2023-02-04 12:26:01 -0800

    first commit

--- test.py
+++ test.py
@@ -1,10 +1,1 @@
-
-import yfinance as yf
-import pandas as pd
-import requests
-from bs4 import BeautifulSoup
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
-print('This is test')
-X = 5
-print('The value of X is', X)
+print("Hello World")
