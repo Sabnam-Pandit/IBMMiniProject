d939b95206e08496d56c768ae7d9abd3c7c5611b
Sabnam Pandit <sabnampandit@Sabnams-Air.lan>
2023-02-04 12:11:31 -0800

    added test

---
+++ test.py
@@ -0,0 +1,10 @@
+
+import yfinance as yf
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+print('This is test')
+X = 5
+print('The value of X is', X)
